7be9dd22143f2d6ec46dc17f17e5e4f4288e66d8
Joel Bakken <joel@>
2017-09-20 08:29:55 -0700

    add yelp api credentials to the project

---
+++ app/src/main/java/com/example/joelbakken/myexercises/Constants.java
@@ -0,0 +1,12 @@
+package com.example.joelbakken.myexercises;
+
+/**
+ * Created by Guest on 9/20/17.
+ */
+
+public class Constants {
+    public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
+    public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
+    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+    public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
+}
